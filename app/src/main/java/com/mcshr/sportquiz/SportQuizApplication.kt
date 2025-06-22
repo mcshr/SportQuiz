0565cfe49daeca7c979fc49d1e11ce049c3f5e1b
mcshr <hruzdovmaksym@gmail.com>
2025-06-22 11:56:17 +0300

    block theme

--- app/src/main/java/com/mcshr/sportquiz/SportQuizApplication.kt
+++ app/src/main/java/com/mcshr/sportquiz/SportQuizApplication.kt
@@ -1,8 +1,13 @@
 package com.mcshr.sportquiz
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SportQuizApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
 }
